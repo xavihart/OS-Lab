ede57684f486e4d7419583119dc082b416296d57
ChrisXue <xavihart@sjtu.edu.cn>
2020-05-20 21:05:49 +0800

    main struct of shell

--- proj2/shell.c
+++ proj2/shell.c
@@ -1,41 +1,56 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<string.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
 #define MAX_LINE 80
-
-int main(){
-	char arg[MAX_LINE / 2 + 1][20];
+ 
+int main(void){
+	char arg[10][MAX_LINE / 2 + 1];
+	char *args[MAX_LINE / 2 + 1];
 	int should_run = 1;
-        int num_arg = 0;
-	while(should_run){
-		printf("osh>");
-		fflush(stdout);
-		for(int i = 0;i < MAX_LINE / 2 + 1;++i){
-		//	arg[i] = NULL;
-		}
-		int num_arg = 0;
-		char tmp[20];
+        int num_arg = 0; 
+        int n = 0;
+	int len = 0;
+        char c;
+      while(should_run){
+        printf("osh>"); 	      
+        pid_t pid;
+	for(int i = 0;i < 10;++i) args[n] = NULL;
+	while(scanf("%s", arg[n])){
 		
-                while(scanf("%s", tmp)){
-                        char c;
-			scanf("%c", &c);
-			//printf("%d", num_arg);
-			//printf("%c", tmp[0]);
-			if(c  == '\n')
-				break;
-			else{
-			    printf("%s\n", tmp);
-			    strcpy(arg[num_arg], tmp);
-			}
-			num_arg += 1;
+                args[n] = arg[n];
+	        n ++;
+		c = getchar();
+		if(c == '\n'){
+		   break;
 		}
-		
-		if(arg[0] == "exit")
-			break;
-	        execvp(arg[0], arg);
-	}
+			
+        }
+      
+      if(strcmp(arg[0], "exit") == 0) should_run = 0;
+      else{
+	  pid = fork();
+          if(pid < 0){
+	      perror("Error:");
+	      return -1;
+	  }	  
+	  else if(pid == 0){
+	      args[n] = NULL;    
+              execvp(args[0], args);
+	      n = 0;
+	      exit(0);
+	  }
+	  else if(pid > 0){
+             n = 0;
+	     wait(NULL);
+	     
+	  }
+      }
+       
 
-	printf("Bye\n");
+   }
 
 	return 0;
 }
