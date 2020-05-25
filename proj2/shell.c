e2b5b3710210a22e90d98ee984e66af8cecb895e
ChrisXue <xavihart@sjtu.edu.cn>
2020-05-25 10:21:50 +0800

    done simple shell

--- proj2/shell.c
+++ proj2/shell.c
@@ -4,8 +4,13 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
 #define MAX_LINE 80
- 
+#define READ_END  0
+#define WRITE_END 1
+
+
 int main(void){
 	int has_hist=0;
 	int hist = 0;
@@ -18,19 +23,36 @@ int main(void){
 	int last_n = 0;
         char c;
 	int wait_=1;
-
+        
+        int use_pipe=0;
+        char *pipes[MAX_LINE / 2 + 1];
+        int pipe_len=0;
+        int pipe_fd[2];
+       
 
 
       while(should_run){
-        printf("osh>"); 	      
+
+
+        printf("osh>"); 
+        fflush(stdout);
+        if(wait_ == 0) wait(NULL);	      
+        
         pid_t pid;
-	for(int i = 0;i < 10;++i) args[n] = NULL;
+        wait_ = 1;
+        n = 0;
+	use_pipe = 0;
+	pipe_len = 0;
+
+       for(int i = 0;i < 10;++i) args[i] = NULL;
 	
 	while(scanf("%s", arg[n])){
 		
                 args[n] = arg[n];
 	        n ++;
-		c = getchar();
+		//c = getchar();
+                scanf("%c", &c);
+
 		if(c == '\n'){
 		   break;
 		}
@@ -42,7 +64,7 @@ int main(void){
 	      continue;
       }
 
-      else if(strcmp(arg[0], "!!") == 0){
+      if(strcmp(arg[0], "!!") == 0){
            hist = 1;
 	   if(!has_hist){
                printf("There is no history command!\n");
@@ -51,67 +73,121 @@ int main(void){
 	       continue;
 	   }
 	   else{
-	       printf("last_n:%d\n",last_n);
+	     //  printf("last_n:%d\n",last_n);
 	       for(int i = 0;i < last_n;++i){
-	           strcpy(args[i], last_args[i]);
-	           //printf("%s ", args[i]);
-		   //printf("\n");
-	       }  
+	           args[i] = last_args[i];
+	           printf("%s ", args[i]);
+	       } 
+               printf("\n");
+               n = last_n; 
 	   }
+      }else{
+         // printf("copy to last:%d\n", n);
+          for(int i = 0;i < n;++i){
+                 strcpy(last_args[i], args[i]); //without the last "NULL"
+          }
+              last_n = n;
       }   
             
           
          // printf("lastn:%d, n:%d\n", last_n, n);
           
-	  if(hist){
-	     args[last_n] = NULL;
-	  }
-	  else{
-             args[n] = NULL;
-	  }
-	  
 	  
                      
-          if(!hist){
-              for(int i = 0;i < n;++i){
-                 strcpy(last_args[i], args[i]); //without the last "NULL"
-                 //printf("%s ", args[i]);
-              }
-              last_n = n;
-              //printf("%d\n", n);
-          }
-
-
- 
 	  hist = 0;	  
 	  has_hist = 1;
-
 // exe part:
 // check < and > and |
+          // check &
+	  if(strcmp(args[n- 1], "&") ==  0){
+                 n -- ;
+                 wait_ = 0;
+                 args[n] = NULL;
+          }
+              
+          // check pipe
+	  for(int i = 0;i < n;++i){
+	      if(strcmp(args[i], "|") == 0){
+	          use_pipe = 1;
+		  args[i] = NULL;
+		  for(int k = i + 1;k < n;++k){
+		      pipes[pipe_len ++] = args[k];
+		  }
+                  n -= pipe_len;
+                 // printf("pipe output : %s, pipe len:%d\n", pipes[0], pipe_len);
+	          break;
+	      }
+	  }
 
-	  pid = fork();
-
+          pid = fork();
+    
           if(pid < 0){
 	      perror("Error:");
 	      return -1;
 	  }	  
 	  else if(pid == 0){
-	      execvp(args[0], args);
-	      exit(0);
+              
+                                        
+              if(n >= 3 && strcmp(args[n - 2], ">") == 0){
+	          int f;
+	          f = open(args[n - 1],  O_CREAT|O_RDWR|O_TRUNC, S_IRUSR|S_IWUSR);
+	          dup2(f, STDOUT_FILENO);
+	          args[n - 1] = NULL;
+	          args[n - 2] = NULL;
+	          n -= 2;	  
+	      }
+	      if(n >= 3 && strcmp(args[n - 2], "<") == 0){
+	          int f2 = open(args[n - 1], O_RDONLY);
+		  dup2(f2, STDIN_FILENO);
+		  args[n - 2] = NULL;
+                  args[n - 1] = NULL;
+                  n -= 2;
+	      }
+              
+        
+              if(use_pipe){   
+                 // printf("use pipe\n");    
+                 if(pipe(pipe_fd) == -1){
+                     fprintf(stderr, "Pipe created failed!------\n");
+                     return 1;
+                 }
+                      
+	         pid_t pipe_pid;
+                 pipe_pid = fork();
+                 if(pipe_pid < 0){
+		      fprintf(stderr, "Fork failed(pipe)\n");
+		      return 1;
+		 }
+                 else if(pipe_pid == 0){
+		      close(pipe_fd[READ_END]);
+		      dup2(pipe_fd[WRITE_END], STDOUT_FILENO);
+		      execvp(args[0], args);
+		      close(pipe_fd[WRITE_END]);
+		      exit(0);
+		 }else{
+		     close(pipe_fd[WRITE_END]);
+		     dup2(pipe_fd[READ_END], STDIN_FILENO);
+		     pipes[pipe_len] = NULL;
+		     execvp(pipes[0], pipes);
+		     close(pipe_fd[READ_END]);
+		     wait(NULL);
+		 }		 
+                  
+              }
+              else{
+                //  printf("not use pipe\n");     
+	          execvp(args[0], args);
+	      }
+              exit(0);
 	  }
-	  else if(pid > 0){
+	  else{
              if(wait_)
-	     wait(NULL);
-	     
+	         wait(NULL);   
 	  }
-
-
- 
-  n = 0;
- 
-   
-  
 }
+
+  
+
 
 	return 0;
 }
